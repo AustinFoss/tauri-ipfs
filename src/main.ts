23785d0921d3f245daaf6ab26f13e7911632c4f0
foss <foss@pop-os.localdomain>
2022-12-13 19:58:49 +0000

    some stuff

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { invoke } from "@tauri-apps/api/tauri";
+import { create } from "ipfs"
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+async function initIpfs() {
+  let node = await create()
+  const id = await node.id()
+  console.log(id)  
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document
+    .querySelector("#greet-button")
+    ?.addEventListener("click", () => greet());
+  document
+    .querySelector("#initIpfs-button")
+    ?.addEventListener("click", () => initIpfs());
+});
---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: ["es2020", "chrome100", "safari13"],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  define: {
+    'process.env.NODE_DEBUG': 'false',
+    'global': 'globalThis'
+  },
+  
+  optimizeDeps: {
+    // enable esbuild dep optimization during build https://github.com/vitejs/vite/issues/9703#issuecomment-1216662109
+    // should be removable with vite 4 https://vitejs.dev/blog/announcing-vite3.html#esbuild-deps-optimization-at-build-time-experimental
+    disabled: false,
+
+    // target: es2020 added as workaround to make big ints work
+    // - should be removable with vite 4
+    // https://github.com/vitejs/vite/issues/9062#issuecomment-1182818044
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  }
+
+
+});
